87fe3b953ed936700185cd11336a8fb8917f2684
Udgeet <bhatt8udgeet@gmail.com>
2024-07-18 22:03:51 +0530

    Implemented Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
+import Button from "@/components/ui/Button"
 import {db} from "@/lib/db"
 
 export default async function Home() {
-  await db.set('hello', 'hello')
-
-  return <div className="text-red-500">Hello World!</div>;
+  return <Button>Hello</Button>
 }

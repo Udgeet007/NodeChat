0276ec779d4100ca039d2e506c43e0cd4fe47513
Udgeet <bhatt8udgeet@gmail.com>
2024-07-18 13:37:39 +0530

    setup.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import Button from "../components/ui/Button";
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button>Hello </Button>
+};
+
+export default page;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+
+
+export default function Home() {
+  return <div className="text-red-500">Hello World!</div>;
+}

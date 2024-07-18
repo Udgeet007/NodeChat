ba4e055bf3f47acaac2ab912003cab7c13de7495
Udgeet <bhatt8udgeet@gmail.com>
2024-07-19 01:39:15 +0530

    Added Users as Friends

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import Button from "@/components/ui/Button"
-import {db} from "@/lib/db"
+import Button from "@/components/ui/Button";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
 
-export default async function Home() {
-  return <Button>Hello</Button>
-}
+const page = async ({}) => {
+  const session = await getServerSession(authOptions);
+
+  return <pre>{JSON.stringify(session)}</pre>;
+};
+
+export default page;

c1214870a583cf21e34f705dde787aeec0093be1
Udgeet <bhatt8udgeet@gmail.com>
2024-07-24 17:01:01 +0530

    chnages

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,49 +1,58 @@
-import { getFriendsByUserId } from '@/helpers/get-friends-by-user-id'
-import { fetchRedis } from '@/helpers/redis'
-import { authOptions } from '@/lib/auth'
-import { chatHrefConstructor } from '@/lib/utils'
-import { ChevronRight } from 'lucide-react'
-import { getServerSession } from 'next-auth'
-import Image from 'next/image'
-import Link from 'next/link'
-import { notFound } from 'next/navigation'
+import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { chatHrefConstructor } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 const page = async ({}) => {
-  const session = await getServerSession(authOptions)
-  if (!session) notFound()
+  const session = await getServerSession(authOptions);
+  if (!session) notFound();
 
-  const friends = await getFriendsByUserId(session.user.id)
+  const friends = await getFriendsByUserId(session.user.id);
 
   const friendsWithLastMessage = await Promise.all(
     friends.map(async (friend) => {
       const [lastMessageRaw] = (await fetchRedis(
-        'zrange',
+        "zrange",
         `chat:${chatHrefConstructor(session.user.id, friend.id)}:messages`,
         -1,
         -1
-      )) as string[]
+      )) as string[];
 
-      const lastMessage = JSON.parse(lastMessageRaw) as Message
+      let lastMessage: Message | null = null;
+
+      if (lastMessageRaw) {
+        try {
+          lastMessage = JSON.parse(lastMessageRaw) as Message;
+        } catch (error) {
+          console.error("Failed to parse lastMessageRaw:", error);
+        }
+      }
 
       return {
         ...friend,
         lastMessage,
-      }
+      };
     })
-  )
+  );
 
   return (
-    <div className='container py-12'>
-      <h1 className='font-bold text-5xl mb-8'>Recent chats</h1>
+    <div className="container py-12">
+      <h1 className="font-bold text-5xl mb-8">Recent chats</h1>
       {friendsWithLastMessage.length === 0 ? (
-        <p className='text-sm text-zinc-500'>Nothing to show here...</p>
+        <p className="text-sm text-zinc-500">Nothing to show here...</p>
       ) : (
         friendsWithLastMessage.map((friend) => (
           <div
             key={friend.id}
-            className='relative bg-zinc-50 border border-zinc-200 p-3 rounded-md'>
-            <div className='absolute right-4 inset-y-0 flex items-center'>
-              <ChevronRight className='h-7 w-7 text-zinc-400' />
+            className="relative bg-zinc-50 border border-zinc-200 p-3 rounded-md"
+          >
+            <div className="absolute right-4 inset-y-0 flex items-center">
+              <ChevronRight className="h-7 w-7 text-zinc-400" />
             </div>
 
             <Link
@@ -51,12 +60,13 @@ const page = async ({}) => {
                 session.user.id,
                 friend.id
               )}`}
-              className='relative sm:flex'>
-              <div className='mb-4 flex-shrink-0 sm:mb-0 sm:mr-4'>
-                <div className='relative h-6 w-6'>
+              className="relative sm:flex"
+            >
+              <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
+                <div className="relative h-6 w-6">
                   <Image
-                    referrerPolicy='no-referrer'
-                    className='rounded-full'
+                    referrerPolicy="no-referrer"
+                    className="rounded-full"
                     alt={`${friend.name} profile picture`}
                     src={friend.image}
                     fill
@@ -65,14 +75,15 @@ const page = async ({}) => {
               </div>
 
               <div>
-                <h4 className='text-lg font-semibold'>{friend.name}</h4>
-                <p className='mt-1 max-w-md'>
-                  <span className='text-zinc-400'>
-                    {friend.lastMessage.senderId === session.user.id
-                      ? 'You: '
-                      : ''}
+                <h4 className="text-lg font-semibold">{friend.name}</h4>
+                <p className="mt-1 max-w-md">
+                  <span className="text-zinc-400">
+                    {friend.lastMessage &&
+                    friend.lastMessage.senderId === session.user.id
+                      ? "You: "
+                      : ""}
                   </span>
-                  {friend.lastMessage.text}
+                  {friend.lastMessage ? friend.lastMessage.text : ""}
                 </p>
               </div>
             </Link>
@@ -80,7 +91,7 @@ const page = async ({}) => {
         ))
       )}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
